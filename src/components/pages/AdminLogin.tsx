9f6e0c912d206464d8ab038f510d3b7115353ce5
royJACK6 <roy.scarafile@gmail.com>
2025-11-10 17:28:11 +0100

    Sgmami Triste

--- src/components/pages/AdminLogin.tsx
+++ src/components/pages/AdminLogin.tsx
@@ -28,7 +28,7 @@ const AdminLogin: React.FC = () => {
       setError('Password non corretta');
       setPassword('');
     }
-  }; //commento per verificare il funzionamento di git
+  }; //Smadonniamo per colpa di Vercel
 
   return (
     <div className="admin-login">
